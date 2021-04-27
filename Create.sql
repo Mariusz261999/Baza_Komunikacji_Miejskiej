fbbc7aa44b5672f6239b21887fb601bd1a40bc1a
Mariusz Nieciecki <mariusz.nieciecki@o2.pl>
2021-04-27 21:46:29 +0200

    Create.sql - poprawka

--- Create.sql
+++ Create.sql
@@ -1,4 +1,6 @@
 ﻿/*
+Created: 21.04.2021
+Modified: 27.04.2021
 Project: Baza Komunikacji Miejskiej
 Company: Politechnika Warszawska
 Database: Oracle 12c
@@ -42,12 +44,42 @@ CREATE INDEX Pracownik_ma_stanowisko ON Pracownicy (ID_stanowiska)
 ALTER TABLE Pracownicy ADD CONSTRAINT Pracownicy_PK PRIMARY KEY (ID_pracownika)
 /
 
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Pracownicy.ID_pracownika IS 'Unikatowy identyfikator pracownika'
+/
+COMMENT ON COLUMN Pracownicy.Imie IS 'Imie pracownika'
+/
+COMMENT ON COLUMN Pracownicy.Nazwisko IS 'Nazwisko pracownika'
+/
+COMMENT ON COLUMN Pracownicy.Data_urodzenia IS 'Data urodzenia pracownika'
+/
+COMMENT ON COLUMN Pracownicy.Plec IS 'Plec pracownika
+'
+/
+COMMENT ON COLUMN Pracownicy.Data_zatrudnienia IS 'Data zatrudnienia pracownika
+'
+/
+COMMENT ON COLUMN Pracownicy.Nr_telefonu IS 'Numer telefonu pracownika
+'
+/
+COMMENT ON COLUMN Pracownicy.Data_zwolnienia IS 'Data zwolnienia pracownika'
+/
+COMMENT ON COLUMN Pracownicy.PESEL IS 'PESEL pracownika'
+/
+COMMENT ON COLUMN Pracownicy.ID_biura IS 'Identyfikator biura FK'
+/
+COMMENT ON COLUMN Pracownicy.ID_adresu IS 'Identyfikator adresu FK'
+/
+COMMENT ON COLUMN Pracownicy.ID_stanowiska IS 'Identyfikator stanowiska FK'
+/
+
 -- Table Biura_Komunikacji_Miejskiej
 
 CREATE TABLE Biura_Komunikacji_Miejskiej(
   ID_biura Integer NOT NULL,
-  Nazwa Varchar2(30 ) NOT NULL,
-  Miasto Varchar2(20 ) NOT NULL,
+  Nazwa Varchar2(50 ) NOT NULL,
+  Miasto Varchar2(30 ) NOT NULL,
   Data_zalozenia Date NOT NULL,
   Strona_WWW Varchar2(50 )
 )
@@ -56,6 +88,23 @@ CREATE TABLE Biura_Komunikacji_Miejskiej(
 -- Add keys for table Biura_Komunikacji_Miejskiej
 
 ALTER TABLE Biura_Komunikacji_Miejskiej ADD CONSTRAINT BiuroKomunikacjiMiejskiej_PK PRIMARY KEY (ID_biura)
+/
+
+ALTER TABLE Biura_Komunikacji_Miejskiej ADD CONSTRAINT Miasto UNIQUE (Miasto)
+/
+
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Biura_Komunikacji_Miejskiej.ID_biura IS 'Unikatowy identyfikator biura'
+/
+COMMENT ON COLUMN Biura_Komunikacji_Miejskiej.Nazwa IS 'Nazwa biura'
+/
+COMMENT ON COLUMN Biura_Komunikacji_Miejskiej.Miasto IS 'Miasto w jakim znajduje się biuro
+'
+/
+COMMENT ON COLUMN Biura_Komunikacji_Miejskiej.Data_zalozenia IS 'Data zalozenia biura'
+/
+COMMENT ON COLUMN Biura_Komunikacji_Miejskiej.Strona_WWW IS 'Adres strony internetowej biura'
 /
 
 -- Table Bilety
@@ -83,6 +132,19 @@ CREATE INDEX IX_Biuro obsluguje bilety ON Bilety (ID_biura)
 ALTER TABLE Bilety ADD CONSTRAINT BiletyPK PRIMARY KEY (ID_biletu)
 /
 
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Bilety.ID_biletu IS 'Unikatowy identyfikator biletu'
+/
+COMMENT ON COLUMN Bilety.Rodzaj_biletu IS 'Rodzaj biletu'
+/
+COMMENT ON COLUMN Bilety.Data_skasowania IS 'Data skasowania biletu'
+/
+COMMENT ON COLUMN Bilety.Id_Klienta IS 'Identyfikator klienta FK'
+/
+COMMENT ON COLUMN Bilety.ID_biura IS 'Identyfikator biura FK'
+/
+
 -- Table Klienci
 
 CREATE TABLE Klienci(
@@ -108,6 +170,21 @@ CREATE INDEX IX_Relationship7 ON Klienci (ID_adresu)
 ALTER TABLE Klienci ADD CONSTRAINT Klient_PK PRIMARY KEY (ID_Klienta)
 /
 
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Klienci.ID_Klienta IS 'Unikatowy identyfikator klienta'
+/
+COMMENT ON COLUMN Klienci.Imie IS 'Imie klienta'
+/
+COMMENT ON COLUMN Klienci.Nazwisko IS 'Nazwisko klienta'
+/
+COMMENT ON COLUMN Klienci.Nr_telefonu IS 'Numer telefonu klienta'
+/
+COMMENT ON COLUMN Klienci.ID_ulgi IS 'Identyfikator ulgi FK'
+/
+COMMENT ON COLUMN Klienci.ID_adresu IS 'Identyfikator adresu FK'
+/
+
 -- Table Pojazdy
 
 CREATE TABLE Pojazdy(
@@ -115,7 +192,7 @@ CREATE TABLE Pojazdy(
   Rodzaj_pojazdu Varchar2(30 ) NOT NULL
         CONSTRAINT Rodzaj_pojazduC CHECK (Rodzaj_pojazdu IN ('AUTOBUS','TRAMWAJ','METRO')),
   Ilosc_miejsc Integer NOT NULL,
-  Rodzaj_napedu Varchar2(15 ) NOT NULL
+  Rodzaj_napedu Varchar2(20 ) NOT NULL
         CONSTRAINT Rodzaj_napeduC CHECK (Rodzaj_napedu IN ('Silnik spalinowy','Hybrydowy','Elektryczny')),
   Rok_produkcji Integer NOT NULL,
   ID_biura Integer NOT NULL
@@ -130,6 +207,21 @@ CREATE INDEX IX_Biuro posiada dany pojazd ON Pojazdy (ID_biura)
 -- Add keys for table Pojazdy
 
 ALTER TABLE Pojazdy ADD CONSTRAINT Pojazdy_PK PRIMARY KEY (ID_pojazdu)
+/
+
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Pojazdy.ID_pojazdu IS 'Unikatowy identyfikator pojazdu'
+/
+COMMENT ON COLUMN Pojazdy.Rodzaj_pojazdu IS 'Rodzaj pojazdu'
+/
+COMMENT ON COLUMN Pojazdy.Ilosc_miejsc IS 'Ilosc miejsc jaka jest dostepna w danym pojezdzie'
+/
+COMMENT ON COLUMN Pojazdy.Rodzaj_napedu IS 'Rodzaj napedu w danym pojezdzie'
+/
+COMMENT ON COLUMN Pojazdy.Rok_produkcji IS 'Rok produkcji pojazdu'
+/
+COMMENT ON COLUMN Pojazdy.ID_biura IS 'Identyfikator biura FK'
 /
 
 -- Table Kursy
@@ -165,6 +257,23 @@ CREATE INDEX IX_Relationship5 ON Kursy (Id_trasy)
 ALTER TABLE Kursy ADD CONSTRAINT KursyPK PRIMARY KEY (ID_kursu)
 /
 
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Kursy.ID_kursu IS 'Unikatowy identyfikator kursu'
+/
+COMMENT ON COLUMN Kursy.Czy_nocny IS 'Czy kurs jest nocny'
+/
+COMMENT ON COLUMN Kursy.Data IS 'Data kursu'
+/
+COMMENT ON COLUMN Kursy.ID_pojazdu IS 'Identyfikator pojazdu FK'
+/
+COMMENT ON COLUMN Kursy.ID_pracownika IS 'Identyfikator pracownika FK'
+/
+COMMENT ON COLUMN Kursy.ID_biura IS 'Identyfikator biura FK'
+/
+COMMENT ON COLUMN Kursy.Id_trasy IS 'Identyfikator trasy FK'
+/
+
 -- Table Przystanki
 
 CREATE TABLE Przystanki(
@@ -182,6 +291,19 @@ CREATE TABLE Przystanki(
 ALTER TABLE Przystanki ADD CONSTRAINT Przystanki_PK PRIMARY KEY (ID_przystanku)
 /
 
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Przystanki.ID_przystanku IS 'Unikatowy identyfikator przystanku'
+/
+COMMENT ON COLUMN Przystanki.Nazwa IS 'Nazwa przystanku'
+/
+COMMENT ON COLUMN Przystanki.Szerokosc_geograficzna IS 'Szerokosc geograficzna przystanku'
+/
+COMMENT ON COLUMN Przystanki.Dlugosc_geograficzna IS 'Dlugosc geograficzna przystanku'
+/
+COMMENT ON COLUMN Przystanki.Na_zadanie IS 'Czy przystanek jest na zadanie'
+/
+
 -- Table Kierowcy
 
 CREATE TABLE Kierowcy(
@@ -193,6 +315,13 @@ CREATE TABLE Kierowcy(
 -- Add keys for table Kierowcy
 
 ALTER TABLE Kierowcy ADD CONSTRAINT Unique_Identifier1 PRIMARY KEY (ID_pracownika)
+/
+
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Kierowcy.ID_pracownika IS 'Identyfikator pracowniak PFK'
+/
+COMMENT ON COLUMN Kierowcy.Data_waznosci_badan IS 'Data ważności badań lekarskich'
 /
 
 -- Table Kontrolerzy
@@ -207,6 +336,15 @@ CREATE TABLE Kontrolerzy(
 -- Add keys for table Kontrolerzy
 
 ALTER TABLE Kontrolerzy ADD CONSTRAINT Unique_Identifier2 PRIMARY KEY (ID_pracownika)
+/
+
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Kontrolerzy.ID_pracownika IS 'Identyfikator pracownika PFK'
+/
+COMMENT ON COLUMN Kontrolerzy.Nr_urzadzenia IS 'Numer urządzenia obsługwanego przez pracownika'
+/
+COMMENT ON COLUMN Kontrolerzy.Premia IS 'Premia dla kontrolera'
 /
 
 -- Table Mandaty
@@ -233,6 +371,20 @@ CREATE INDEX IX_Kontrolet_wystawil ON Mandaty (ID_pracownika)
 ALTER TABLE Mandaty ADD CONSTRAINT Mandat_PK PRIMARY KEY (ID_mandatu)
 /
 
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Mandaty.ID_mandatu IS 'Unikatowy identyfikator mandatu'
+/
+COMMENT ON COLUMN Mandaty.Wysokosc_mandatu IS 'Wysokosc wystawionego mandatu
+'
+/
+COMMENT ON COLUMN Mandaty.Data IS 'Data wystawienia mandatu'
+/
+COMMENT ON COLUMN Mandaty.Id_Klienta IS 'Identyfikator klienta FK'
+/
+COMMENT ON COLUMN Mandaty.ID_pracownika IS 'Identyfikator pracownika FK'
+/
+
 -- Table Trasy
 
 CREATE TABLE Trasy(
@@ -245,6 +397,8 @@ CREATE TABLE Trasy(
 
 COMMENT ON COLUMN Trasy.ID_trasy IS 'Unikatowy identyfikator trasy'
 /
+COMMENT ON COLUMN Trasy.Nazwa IS 'Nazwa trasy'
+/
 
 -- Table Pracownicy_Pojazdy
 
@@ -254,12 +408,19 @@ CREATE TABLE Pracownicy_Pojazdy(
 )
 /
 
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Pracownicy_Pojazdy.ID_pracownika IS 'Identyfikator pracownika PFK'
+/
+COMMENT ON COLUMN Pracownicy_Pojazdy.ID_pojazdu IS 'Identyfikator pojazdu PFK'
+/
+
 -- Table Ulgi
 
 CREATE TABLE Ulgi(
   ID_ulgi Integer NOT NULL,
   Nazwa Varchar2(40 ) NOT NULL,
-  Wysokosc Varchar2(3 ) NOT NULL,
+  Wysokosc Varchar2(4 ) NOT NULL,
   Opis Varchar2(200 ) NOT NULL
 )
 /
@@ -276,9 +437,11 @@ ALTER TABLE Ulgi ADD CONSTRAINT Opis UNIQUE (Opis)
 
 COMMENT ON COLUMN Ulgi.ID_ulgi IS 'Unikatowy identyfikator ulgi'
 /
-COMMENT ON COLUMN Ulgi.Nazwa IS 'Unikatowa nazwa ulgi'
+COMMENT ON COLUMN Ulgi.Nazwa IS 'Nazwa ulgi'
 /
 COMMENT ON COLUMN Ulgi.Wysokosc IS 'Wysokosc ulgi'
+/
+COMMENT ON COLUMN Ulgi.Opis IS 'Opis do kogo skierowana jest ulga'
 /
 
 -- Table Adresy
@@ -306,13 +469,15 @@ ALTER TABLE Adresy ADD CONSTRAINT PK_Adresy PRIMARY KEY (ID_adresu)
 
 COMMENT ON TABLE Adresy IS 'Encja Adresy'
 /
-COMMENT ON COLUMN Adresy.ID_adresu IS 'Unikatowy klucz glowny encji Adresy'
+COMMENT ON COLUMN Adresy.ID_adresu IS 'Unikatowy identyfikator adresu'
 /
-COMMENT ON COLUMN Adresy.Miasto IS 'Atrybut miasto '
+COMMENT ON COLUMN Adresy.Miasto IS 'Miasto'
 /
 COMMENT ON COLUMN Adresy.Ulica IS 'Ulica'
 /
-COMMENT ON COLUMN Adresy.Nr_lokalu IS 'Atrybut numer lokalu'
+COMMENT ON COLUMN Adresy.Nr_lokalu IS 'Numer lokalu'
+/
+COMMENT ON COLUMN Adresy.ID_poczty IS 'Identyfikator poczty FK'
 /
 
 -- Table Poczty
@@ -359,16 +524,13 @@ CREATE TABLE Stanowiska(
 ALTER TABLE Stanowiska ADD CONSTRAINT PK_Stanowiska PRIMARY KEY (ID_stanowiska)
 /
 
-ALTER TABLE Stanowiska ADD CONSTRAINT Nazwa UNIQUE (Nazwa)
-/
-
 -- Table and Columns comments section
 
 COMMENT ON TABLE Stanowiska IS 'Relacja slownikowa stanowiska'
 /
 COMMENT ON COLUMN Stanowiska.ID_stanowiska IS 'Unikatowy indentyfikator stanowiska'
 /
-COMMENT ON COLUMN Stanowiska.Nazwa IS 'Unikatowa nazwa stanowiska'
+COMMENT ON COLUMN Stanowiska.Nazwa IS 'Nazwa stanowiska'
 /
 COMMENT ON COLUMN Stanowiska.Opis IS 'Opis stanowiska'
 /
@@ -407,9 +569,13 @@ COMMENT ON TABLE Prawa_jazdy IS 'Encja prawa jazdy'
 /
 COMMENT ON COLUMN Prawa_jazdy.ID_prawo_jazdy IS 'Unikatowy identyfikator prawo jazdy'
 /
+COMMENT ON COLUMN Prawa_jazdy.Nr_prawa_jazdy IS 'Numer prawa jazdy'
+/
 COMMENT ON COLUMN Prawa_jazdy.Data_wydania IS 'Data wydania prawo jazdy'
 /
 COMMENT ON COLUMN Prawa_jazdy.Data_waznosci IS 'Data waznosci prawo jazdy'
+/
+COMMENT ON COLUMN Prawa_jazdy.ID_pracownika IS 'Identyfikator pracownika FK'
 /
 
 -- Table Wynagrodzenia
@@ -435,9 +601,15 @@ ALTER TABLE Wynagrodzenia ADD CONSTRAINT PK_Wynagrodzenia PRIMARY KEY (ID_wynagr
 
 -- Table and Columns comments section
 
-COMMENT ON COLUMN Wynagrodzenia.ID_wynagrodzenia IS 'Klucz unikatowy wynagrodzenia'
+COMMENT ON COLUMN Wynagrodzenia.ID_wynagrodzenia IS 'Unikatowy identyfikator biura'
 /
 COMMENT ON COLUMN Wynagrodzenia.Data IS 'Data wynagrodzenia'
+/
+COMMENT ON COLUMN Wynagrodzenia.Kwota_podstawowa IS 'Kwota podstawowa wynagrodzenia'
+/
+COMMENT ON COLUMN Wynagrodzenia.Kwota_dodatkowa IS 'Kwota dodatkowa'
+/
+COMMENT ON COLUMN Wynagrodzenia.ID_pracownika IS 'Identyfikator pracownika FK'
 /
 
 -- Table Trasy_przystanki
@@ -452,6 +624,15 @@ CREATE TABLE Trasy_przystanki(
 -- Add keys for table Trasy_przystanki
 
 ALTER TABLE Trasy_przystanki ADD CONSTRAINT PK_Trasy_przystanki PRIMARY KEY (Id_trasy,ID_przystanku)
+/
+
+-- Table and Columns comments section
+
+COMMENT ON COLUMN Trasy_przystanki.Id_trasy IS 'Identyfikator trasy PFK'
+/
+COMMENT ON COLUMN Trasy_przystanki.ID_przystanku IS 'Identyfikator przystanku PFK'
+/
+COMMENT ON COLUMN Trasy_przystanki.Kolejnosc IS 'Kolejnosc przystankow na trasie '
 /
 
 
